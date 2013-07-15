9f9c13946459854f582ee2a076397cf138c1b079
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-15 14:08:10 +0200

    First commit

---
+++ src/MetaConsole/Application.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @author matthieu.napoli
+ */
+
+namespace MetaConsole;
+
+use MetaModel\MetaModel;
+use Symfony\Component\Console\Input\InputInterface;
+
+/**
+ * Extending the Symfony Console Application to allow having one command only
+ *
+ * @see http://symfony.com/doc/current/components/console/single_command_tool.html
+ */
+class Application extends \Symfony\Component\Console\Application
+{
+    /**
+     * @var MetaModel
+     */
+    private $metaModel;
+
+    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN', MetaModel $metaModel = null)
+    {
+        $this->metaModel = $metaModel ?: new MetaModel();
+
+        parent::__construct($name, $version);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCommandName(InputInterface $input)
+    {
+        return 'run';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultCommands()
+    {
+        // Keep the core default commands to have the HelpCommand
+        // which is used when using the --help option
+        $defaultCommands = parent::getDefaultCommands();
+
+        $runCommand = new RunCommand();
+        $runCommand->setMetaModel($this->metaModel);
+
+        $defaultCommands[] = $runCommand;
+
+        return $defaultCommands;
+    }
+
+    /**
+     * Overridden so that the application doesn't expect the command
+     * name to be the first argument
+     */
+    public function getDefinition()
+    {
+        $inputDefinition = parent::getDefinition();
+        // clear out the normal first argument, which is the command name
+        $inputDefinition->setArguments();
+
+        return $inputDefinition;
+    }
+}
