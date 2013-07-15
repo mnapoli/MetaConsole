4dec039c84e16de7329908a22e9fc11cf0d62192
Matthieu Napoli <matthieu@mnapoli.fr>
2013-07-15 21:19:16 +0200

    Demo

---
+++ demo/Bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\Setup;
+
+require_once __DIR__ . '/Model/Article.php';
+require_once __DIR__ . '/Model/Category.php';
+
+class Bootstrap
+{
+	/**
+	 * @var EntityManager
+	 */
+	private $entityManager;
+
+	public function __construct() {
+		$dbParams = array(
+			'dbname' => 'demo',
+			'memory' => 'true',
+			'driver' => 'pdo_sqlite',
+		);
+		$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . '/Model'), true);
+		$this->entityManager = EntityManager::create($dbParams, $config);
+		$tool = new SchemaTool($this->entityManager);
+		$tool->createSchema($this->entityManager->getMetadataFactory()->getAllMetadata());
+
+        // Create data
+		$article = new Article(1);
+		$this->entityManager->persist($article);
+        $article = new Article(2);
+        $this->entityManager->persist($article);
+
+		$this->entityManager->flush();
+	}
+
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->entityManager;
+    }
+}
---
+++ demo/Model/Article.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @Entity
+ */
+class Article
+{
+	/**
+	 * @Id @Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @ManyToOne(targetEntity="Category", inversedBy="articles")
+	 **/
+	private $category;
+
+	public function __construct($id) {
+		$this->id = $id;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getCategory() {
+		return $this->category;
+	}
+
+	public function setCategory(Category $category) {
+		$this->category = $category;
+	}
+}
